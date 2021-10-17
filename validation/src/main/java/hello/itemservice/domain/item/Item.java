ac02f9cf65211736685782fe5738bfdd590c9be8
박경탁 <qkrrudxkr77@naver.com>
2021-10-17 16:02:30 +0900

    # 검증2 - Bean Validation ## Bean Validation - 한계

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,6 +2,7 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
@@ -10,25 +11,26 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Item {
 
-    private Long id;
+  @NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
+  private Long id;
 
-    @NotBlank
-    private String itemName;
+  @NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+  private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000)
-    private Integer price;
+  @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+  @Range(min = 1000, max = 1000000, groups = {SaveCheck.class, UpdateCheck.class})
+  private Integer price;
 
-    @NotNull
-    @Max(9999)
-    private Integer quantity;
+  @NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+  @Max(value = 9999, groups = SaveCheck.class)
+  private Integer quantity;
 
-    public Item() {
-    }
+  public Item() {
+  }
 
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
+  public Item(String itemName, Integer price, Integer quantity) {
+    this.itemName = itemName;
+    this.price = price;
+    this.quantity = quantity;
+  }
 }
